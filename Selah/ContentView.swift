153924cd8461c30e8957e95beeb5fb5f039e917c
James Park <jameshojungpark@gmail.com>
2026-04-08 10:06:26 -0700

    Initial Commit

---
+++ Selah/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Selah
+//
+//  Created by 박호정 on 2026-03-24.
+// Testing github
+
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
