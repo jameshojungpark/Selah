e30a2459c8cf63c56a0121279f09c8138e7f586c
James Park <jameshojungpark@gmail.com>
2026-04-08 10:54:12 -0700

    feat: create AppLimit domain entity

--- Selah/Domain/Entities/AppLimit.swift
+++ Selah/Domain/Entities/AppLimit.swift
@@ -1,8 +1,22 @@
 //
-//  File.swift
+//  AppLimit.swift
 //  Selah
 //
-//  Created by 박호정 on 2026-04-08.
+//  Created by James Park on 2026-04-08.
 //
 
 import Foundation
+
+struct AppLimit: Identifiable, Equatable{
+    let id: UUID
+    var bundleIdentifier: String
+    var dailyMinutes: Int
+    var isActive: Bool
+    
+    init(id: UUID = UUID(), bundleIdentifier: String, dailyMinutes: Int, isActive: Bool = true){
+        self.id = id
+        self.bundleIdentifier = bundleIdentifier
+        self.dailyMinutes = dailyMinutes
+        self.isActive = isActive
+    }
+}
